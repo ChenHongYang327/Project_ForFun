10556622999e39b40260cac7a47f675ddd9c8e08
王威力 <lkk7890j35@gmail.com>
2021-06-16 14:51:15 +0800

    新增發文,	詳細layout,修改itemview layout

---
+++ app/src/main/java/idv/tfp10105/project_forfun/discussionboard/controller/discussionDetailFragment.java
@@ -0,0 +1,26 @@
+package idv.tfp10105.project_forfun.discussionboard.controller;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import idv.tfp10105.project_forfun.R;
+
+public class discussionDetailFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_discussion_detail, container, false);
+    }
+}
