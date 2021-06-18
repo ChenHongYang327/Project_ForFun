495d867b068fb49b16f5dfec278006e5d3602e25
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-06-18 18:47:46 +0800

    新增 orderconfirm 合約頁面的UI (Orderconfirm_agreement)

---
+++ app/src/main/java/idv/tfp10105/project_forfun/orderconfirm/Orderconfirm_agreement.java
@@ -0,0 +1,37 @@
+package idv.tfp10105.project_forfun.orderconfirm;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.jetbrains.annotations.NotNull;
+
+import idv.tfp10105.project_forfun.R;
+
+public class Orderconfirm_agreement extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_orderconfirm_agreement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}
