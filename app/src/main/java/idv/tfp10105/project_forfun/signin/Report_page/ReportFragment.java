af1b3288e4620c3b33347251be4bc0aa821a585a
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-08-11 10:15:05 +0800

    調整 reoort & agreement UI

--- app/src/main/java/idv/tfp10105/project_forfun/signin/Report_page/ReportFragment.java
+++ app/src/main/java/idv/tfp10105/project_forfun/signin/Report_page/ReportFragment.java
@@ -181,13 +181,13 @@ public class ReportFragment extends Fragment {
                        Log.d("HI", jsonIn);
                        JsonObject jObject = new Gson().fromJson(jsonIn, JsonObject.class);
                        if (jObject.get("status").getAsBoolean()) {
-                           Toast.makeText(activity, "新增成功", Toast.LENGTH_SHORT).show();
+                           Toast.makeText(activity, "檢舉成功", Toast.LENGTH_SHORT).show();
                            Navigation.findNavController(v).popBackStack();
                        } else {
-                           Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                           Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                        }
                    }else {
-                           Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                           Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                        }
 
                 break;
@@ -211,13 +211,13 @@ public class ReportFragment extends Fragment {
                         String jsonIn = RemoteAccess.getJsonData(url, jsonObject.toString());
                         JsonObject jObject = new Gson().fromJson(jsonIn, JsonObject.class);
                         if (jObject.get("status").getAsBoolean()) {
-                            Toast.makeText(activity, "新增成功", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(activity, "檢舉成功", Toast.LENGTH_SHORT).show();
                             Navigation.findNavController(v).popBackStack();
                         } else {
-                            Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                         }
                     }else {
-                        Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                     }
                     break;
 
@@ -239,13 +239,13 @@ public class ReportFragment extends Fragment {
                         String jsonIn = RemoteAccess.getJsonData(url, jsonObject.toString());
                         JsonObject jObject = new Gson().fromJson(jsonIn, JsonObject.class);
                         if (jObject.get("status").getAsBoolean()) {
-                            Toast.makeText(activity, "新增成功", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(activity, "檢舉成功", Toast.LENGTH_SHORT).show();
                             Navigation.findNavController(v).popBackStack();
                         } else {
-                            Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                         }
                     }else {
-                        Toast.makeText(activity, "新增失敗", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(activity, "檢舉失敗", Toast.LENGTH_SHORT).show();
                     }
 
                     break;
@@ -258,11 +258,6 @@ public class ReportFragment extends Fragment {
 
             }
         });
-
-
-
-
-
 
 //        //按鈕 取消
         btCancel.setOnClickListener(v -> {
